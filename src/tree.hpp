d9181b98dea9e38f9020801e9fbd437940ad652b
Sean <sturner@nmt.edu>
2014-11-15 18:37:53 -0700

    remove hash as a member

--- src/tree.hpp
+++ src/tree.hpp
@@ -14,15 +14,11 @@ namespace tree {
   template<typename T>
   class BasicTreeNode {
   private:
-    
-    //! hash of left node concat hash of right node
-    std::unique_ptr<std::string> _hash;
-    
     //! will be null for nonleaf nodes
-    std::unique_ptr<T> _payload;
+    std::unique_ptr<T> payload;
 
-    std::unique_ptr<BasicTreeNode> _left;
-    std::unique_ptr<BasicTreeNode> _right;
+    std::unique_ptr<BasicTreeNode> left;
+    std::unique_ptr<BasicTreeNode> right;
 
   public:
     BasicTreeNode();
@@ -33,24 +29,22 @@ namespace tree {
     std::string getHash();
     
     //! getter and setter for payload
-    template<typename T>
+    //template<typename T>
     std::unique_ptr<T> getPayload();
     
-    template<typename T>
+    //template<typename T>
     void setPayload(const T& payload);
     
     //! getter and setter for left child
-    template<typename T>
+    //template<typename T>
     std::unique_ptr<T> getLeftChild();
     
-    template<typename T>
     void setLeftChild(const BasicTreeNode& node);
     
     //! getter and setter for right child
-    template<typename T>
+    //template<typename T>
     std::unique_ptr<T> getRightChild();
     
-    template<typename T>
     void setRightChild(const BasicTreeNode& node);
 
     void addTransaction();
