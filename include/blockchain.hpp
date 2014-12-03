73eef8a58915c97959773c5429427f6e57c2ca8b
Sean <sturner@nmt.edu>
2014-12-02 20:48:15 -0700

    quick n dirty fake blockchain started

---
+++ include/blockchain.hpp
@@ -0,0 +1,15 @@
+/**
+ * blockchain.hpp
+ * 
+ * A simple hacky "blockchain" in order to demonstrate
+ * FunCoin
+ *
+ */
+
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
