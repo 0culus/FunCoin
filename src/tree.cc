5e922ceffaf5c1e2b054e17427b706dc8562b5cb
Sean <sturner@nmt.edu>
2014-12-30 15:45:24 -0700

    fix + move implementation to header

--- src/tree.cc
+++ src/tree.cc
@@ -9,82 +9,33 @@
 
 using namespace tree;
 
-//! make sure payload is null for a new node
-template<typename T>
-BasicTreeNode<T>::BasicTreeNode() {
-  this->payload = nullptr;
-}
-
-template<typename T>
-std::string BasicTreeNode<T>::getHash() {
-  
-  //! cases:
-  // node is a leaf, so hash is the transaction
-  if (!left && !right) {
-    std::stringstream s;
-    s << this->payload;
-    return s.str();
-  }
-  
-  // node is not a leaf, but left child exists
-  // so we concatenate the existing hash with
-  // itself
-  else if (left && !right) {
-    return this->left.getHash() + this->left.getHash();
-  }
-  
-  // node is not a leaf, but right child exists
-  else if (!left && right) {
-    return this->right.getHash() + this->right.getHash();
-  }
-  
-  // node is not a leaf, but has both children
-  else {
-    return this->left.getHash() + this->right.getHash();
-  }
-}
-
-template<typename T>
-decltype(auto) BasicTreeNode<T>::getPayload() {
-  return this->payload;
-}
-
-template<typename T>
-void BasicTreeNode<T>::setPayload(const T& payload) {
-  this->payload = std::make_unique<T>(payload);
-}
-
-template<typename T>
-decltype(auto) BasicTreeNode<T>::getLeftChild() {
-  return this->left;
-}
-
-template<typename T>
-void BasicTreeNode<T>::setLeftChild(const BasicTreeNode& node) {
-  this->left = node;
-}
-
-template<typename T>
-decltype(auto) BasicTreeNode<T>::getRightChild() {
-  return this->right;
-}
-
-template<typename T>
-void BasicTreeNode<T>::setRightChild(const BasicTreeNode& node) {
-  this->right = node;
-}
+#ifdef DEBUG
+  using boost::typeindex::type_id_with_cvr;
+#endif
 
 int main(void) {
-  auto t1 = "alice-sends-bob-5-funcoin";
-  auto t2 = "trent-sends-alice-2-funcoin";
-  auto t3 = "bob-sends-alice-10-funcoin";
-  auto t4 = "trent-sends-bob-3-funcoin";
+  std::vector<std::string> transactions = { "alice-sends-bob-5-funcoin",
+                                  "trent-sends-alice-2-funcoin",
+                                  "bob-sends-alice-10-funcoin",
+                                  "trent-sends-bob-3-funcoin" };
   
   // create root node
   decltype(auto) MerkleRoot = new BasicTreeNode<std::string>();
-  MerkleRoot->setPayload(t1);
+  std::cout << "MerkleRoot's type =    "
+  << type_id_with_cvr<decltype(MerkleRoot)>().pretty_name();
+  
+  auto str = transactions[0];
+  
+  decltype(auto) t1 = std::make_unique<std::string>(str);
+  std::cout << "t1's type =    "
+  << type_id_with_cvr<decltype(t1)>().pretty_name();
+  
+  MerkleRoot->setPayload(*t1);
   
   decltype(auto) MerkleRootPtr = std::make_unique<BasicTreeNode<std::string>>(*MerkleRoot);
+  std::cout << "MerkleRootPtr's type =    "
+  << type_id_with_cvr<decltype(MerkleRootPtr)>().pretty_name();
+  
   
   return 0;
 }
--- src/tree.hpp
+++ src/tree.hpp
@@ -7,12 +7,18 @@
 
 #pragma once
 
+#define DEBUG 1
+
 #include <memory>
 #include <iomanip>
 #include <vector>
 #include <iostream>
 #include <string>
 #include <sstream>
+
+#ifdef DEBUG
+  #include <boost/type_index.hpp>
+#endif
 
 namespace tree {
 
@@ -27,8 +33,8 @@ namespace tree {
 
   public:
     BasicTreeNode();
-    BasicTreeNode(const BasicTreeNode& other);
-    ~BasicTreeNode();
+    BasicTreeNode(const BasicTreeNode& other) = default;
+    ~BasicTreeNode() = default;
     
     //! getter and setter for hash
     std::string getHash();
@@ -51,6 +57,71 @@ namespace tree {
 
     void addTransaction();
   };
+  
+  //! make sure payload is null for a new node
+  template<typename T>
+  BasicTreeNode<T>::BasicTreeNode() {
+    this->payload = nullptr;
+  }
+  
+  template<typename T>
+  std::string BasicTreeNode<T>::getHash() {
+    
+    //! cases:
+    // node is a leaf, so hash is the transaction
+    if (!left && !right) {
+      std::stringstream s;
+      s << this->payload;
+      return s.str();
+    }
+    
+    // node is not a leaf, but left child exists
+    // so we concatenate the existing hash with
+    // itself
+    else if (left && !right) {
+      return this->left.getHash() + this->left.getHash();
+    }
+    
+    // node is not a leaf, but right child exists
+    else if (!left && right) {
+      return this->right.getHash() + this->right.getHash();
+    }
+    
+    // node is not a leaf, but has both children
+    else {
+      return this->left.getHash() + this->right.getHash();
+    }
+  }
+  
+  template<typename T>
+  decltype(auto) BasicTreeNode<T>::getPayload() {
+    return this->payload;
+  }
+  
+  template<typename T>
+  void BasicTreeNode<T>::setPayload(const T& payload) {
+    this->payload = std::make_unique<T>(payload);
+  }
+  
+  template<typename T>
+  decltype(auto) BasicTreeNode<T>::getLeftChild() {
+    return this->left;
+  }
+  
+  template<typename T>
+  void BasicTreeNode<T>::setLeftChild(const BasicTreeNode& node) {
+    this->left = node;
+  }
+  
+  template<typename T>
+  decltype(auto) BasicTreeNode<T>::getRightChild() {
+    return this->right;
+  }
+  
+  template<typename T>
+  void BasicTreeNode<T>::setRightChild(const BasicTreeNode& node) {
+    this->right = node;
+  }
 
 }
 
