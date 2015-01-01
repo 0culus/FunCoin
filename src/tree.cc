cf0335857b171ffd974a10ffb7dd6d5f4cd42495
Sean <sturner@nmt.edu>
2015-01-01 12:53:24 -0700

    fix ifdefs

--- src/tree.cc
+++ src/tree.cc
@@ -21,23 +21,31 @@ int main(void) {
   
   // create root node
   decltype(auto) MerkleRoot = new BasicTreeNode<std::string>();
+  
+#ifdef DEBUG
   std::cout << "MerkleRoot's type =    "
   << type_id_with_cvr<decltype(MerkleRoot)>().pretty_name();
+#endif
   
   auto str = transactions[0];
   
   decltype(auto) t1 = std::make_unique<std::string>(str);
+  
+#ifdef DEBUG
   std::cout << "t1's type =    "
   << type_id_with_cvr<decltype(t1)>().pretty_name();
+#endif
   
   MerkleRoot->setPayload(*t1);
   
   auto MerkleRootRvalue = std::move(MerkleRoot);
   
   decltype(auto) MerkleRootPtr = std::make_unique<BasicTreeNode<std::string>>(*MerkleRootRvalue);
+  
+#ifdef DEBUG
   std::cout << "MerkleRootPtr's type =    "
   << type_id_with_cvr<decltype(MerkleRootPtr)>().pretty_name();
-  
+#endif
   
   return 0;
 }
